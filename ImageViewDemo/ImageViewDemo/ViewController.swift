c0698052124929db8769ca86ed2bd4cdf839a1c7
Steven Ybarra <Smybarr@gmail.com>
2018-10-01 14:12:52 -0500

    Created Demo App - ImageViewDemo
    ImageViewDemo just shows a basic image taken from the internet with a UI label that describes how to add an image view to an XCode project.

---
+++ ImageViewDemo/ImageViewDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ImageViewDemo
+//
+//  Created by Steven Ybarra on 10/1/18.
+//  Copyright © 2018 Smybarr. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
